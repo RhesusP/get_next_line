6fa53b91bdc5ef2deb72d0809baff6b066d9adad
RhesusP <camil.bernot.cb@gmail.com>
2022-11-23 20:29:39 +0100

    header and utils norminette friendly

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: cbernot <cbernot@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 09:18:54 by cbernot           #+#    #+#             */
-/*   Updated: 2022/11/23 17:22:35 by cbernot          ###   ########.fr       */
+/*   Updated: 2022/11/23 20:28:50 by cbernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,15 @@
 # include <stdlib.h>
 # include <stdio.h>	//TODO remove include
 
-
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1
 # endif
 
-char    *get_next_line(int fd);
+char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
 char	*ft_strjoin(char const *s1, char const *s2);
-char    *ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 void	*ft_free_stash(char **stash);
-
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: cbernot <cbernot@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 09:36:53 by cbernot           #+#    #+#             */
-/*   Updated: 2022/11/23 18:02:16 by cbernot          ###   ########.fr       */
+/*   Updated: 2022/11/23 20:28:43 by cbernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,29 +68,29 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	return (res);
 }
 
-char    *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-        char    *res;
-        size_t  i;
-        size_t  j;
+	char	*res;
+	size_t	i;
+	size_t	j;
 
-        if (!s)
-                return (0);
-        if (len > ft_strlen(s))
-                len = ft_strlen(s);
-        res = malloc(sizeof(char) * (len + 1));
-        if (!res || !s)
-                return (0);
-        i = start;
-        j = 0;
-        while (i < ft_strlen(s) && j < len)
-        {
-                res[j] = s[i];
-                i++;
-                j++;
-        }
-        res[j] = '\0';
-        return (res);
+	if (!s)
+		return (0);
+	if (len > ft_strlen(s))
+		len = ft_strlen(s);
+	res = malloc(sizeof(char) * (len + 1));
+	if (!res || !s)
+		return (0);
+	i = start;
+	j = 0;
+	while (i < ft_strlen(s) && j < len)
+	{
+		res[j] = s[i];
+		i++;
+		j++;
+	}
+	res[j] = '\0';
+	return (res);
 }
 
 void	*ft_free_stash(char **stash)
